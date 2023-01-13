8240f5c80f8a633b3910965367b301354a2e5035
MJackson22-bit <jm5444859@gmail.com>
2023-01-12 21:39:42 -0600

    feat: Added base class for Fragments and Activities

---
+++ app/src/main/java/com/example/napets/core/base/BaseFragment.kt
@@ -0,0 +1,32 @@
+package com.example.napets.core.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment<B: ViewBinding> : Fragment() {
+
+    protected lateinit var binding: B
+    protected abstract fun getViewBinding(): B
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        init()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return binding.root
+    }
+
+    private fun init() {
+        binding = getViewBinding()
+    }
+
+}
