95b14f524ed1487ad49a853812bd5647f3a78652
MJackson22-bit <jm5444859@gmail.com>
2023-01-12 22:59:11 -0600

    delete HiltViewModel

--- app/src/main/java/com/example/napets/ui/landing/viewmodel/LandingViewModel.kt
+++ app/src/main/java/com/example/napets/ui/landing/viewmodel/LandingViewModel.kt
@@ -3,7 +3,6 @@ package com.example.napets.ui.landing.viewmodel
 import com.example.napets.core.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 
-@HiltViewModel
 class LandingViewModel : BaseViewModel() {
 
 }
--- app/src/main/java/com/example/napets/ui/main/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/napets/ui/main/viewmodel/MainViewModel.kt
@@ -3,7 +3,6 @@ package com.example.napets.ui.main.viewmodel
 import com.example.napets.core.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 
-@HiltViewModel
 class MainViewModel : BaseViewModel() {
 
 }
