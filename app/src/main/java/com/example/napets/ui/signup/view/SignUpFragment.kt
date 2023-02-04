d84429674975bdfdcf9ae36b3bf5ea7f3552aac7
MJackson22-bit <jm5444859@gmail.com>
2023-02-04 14:15:51 -0600

    Feat: Add FragmentSignUp to NavGraph Login
    Add Safe Args dependency

--- app/src/main/java/com/example/napets/ui/signup/view/SignUpFragment.kt
+++ app/src/main/java/com/example/napets/ui/signup/view/SignUpFragment.kt
@@ -1,22 +1,22 @@
-package com.example.napets.ui.signin.view
+package com.example.napets.ui.signup.view
 
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
-import com.example.napets.databinding.FragmentSignInBinding
+import com.example.napets.databinding.FragmentSignUpBinding
 import com.example.napets.ui.base.BaseFragment
 import com.example.napets.ui.main.viewmodel.MainViewModel
-import com.example.napets.ui.signin.viewmodel.SigInViewModel
+import com.example.napets.ui.signup.viewmodel.SigInViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class SignInFragment : BaseFragment<FragmentSignInBinding>() {
+class SignUpFragment : BaseFragment<FragmentSignUpBinding>() {
 
     private val viewModel: SigInViewModel by viewModels()
     private val mainViewModel: MainViewModel by activityViewModels()
 
-    override fun getViewBinding() = FragmentSignInBinding.inflate(layoutInflater)
+    override fun getViewBinding() = FragmentSignUpBinding.inflate(layoutInflater)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
