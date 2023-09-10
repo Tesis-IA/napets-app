47c2ecedd1a68e4a82f176ade4612335ea8e0d52
MJackson22-bit <jm5444859@gmail.com>
2023-09-10 00:39:00 -0600

    Fix issues founded for detek

--- app/src/main/java/com/quantumcode/napets/ui/myProfile/view/HomeProfileFragment.kt
+++ app/src/main/java/com/quantumcode/napets/ui/myProfile/view/HomeProfileFragment.kt
@@ -1,11 +1,5 @@
-package com.quantumcode.napets.ui.my_profile.view
+package com.quantumcode.napets.ui.myProfile.view
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.quantumcode.napets.R
 import com.quantumcode.napets.databinding.FragmentHomeProfileBinding
 import com.quantumcode.napets.ui.base.BaseFragment
 
