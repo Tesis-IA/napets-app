965d30e4191c331ab81004de041a805bdbfb8728
MJackson22-bit <jm5444859@gmail.com>
2024-02-17 21:59:00 -0600

    fix detek problems

--- app/src/main/java/com/quantumcode/napets/ui/diagnosis/adapter/DiagnosisInfoAdapter.kt
+++ app/src/main/java/com/quantumcode/napets/ui/diagnosis/adapter/DiagnosisInfoAdapter.kt
@@ -1,6 +1,5 @@
 package com.quantumcode.napets.ui.diagnosis.adapter
 
-import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
